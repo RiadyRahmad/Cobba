10bd6f7ae23745b51d29e8174f73baf831773d6c
Riady Rahmad K <riadyrahmad92@gmail.com>
2019-07-22 10:29:04 +0700

    new

---
+++ coba/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Jumbotron } from 'react-bootstrap';
+// import content from './content.';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
